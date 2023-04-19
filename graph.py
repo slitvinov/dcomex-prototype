20ddd78c10dd86d857fd02e975a2c487d05652e5
Sergey Litvinov <slitvinov@gmail.com>
2023-04-19 15:27:22 -0400

    graph.py: samplers use tuple as arguments

--- graph.py
+++ graph.py
@@ -96,7 +96,7 @@ def metropolis(fun, draws, init, scale, log=False):
         t += 1
         if t == draws:
             break
-        xp = [e + random.gauss(0, s) for e, s in zip(x, scale)]
+        xp = tuple(e + random.gauss(0, s) for e, s in zip(x, scale))
         pp = fun(xp)
         if pp > p or cond(pp, p):
             x, p = xp, pp
@@ -143,7 +143,7 @@ def langevin(fun, draws, init, dfun, sigma, log=False):
 
     s2 = sigma * sigma
     x = init[:]
-    y = [x + s2 / 2 * d for x, d in zip(x, dfun(x))]
+    y = tuple(x + s2 / 2 * d for x, d in zip(x, dfun(x)))
     p = fun(x)
     t = 0
     accept = 0
@@ -153,8 +153,8 @@ def langevin(fun, draws, init, dfun, sigma, log=False):
         t += 1
         if t == draws:
             break
-        xp = [random.gauss(y, sigma) for y in y]
-        yp = [xp + s2 / 2 * d for xp, d in zip(xp, dfun(xp))]
+        xp = tuple(random.gauss(y, sigma) for y in y)
+        yp = tuple(xp + s2 / 2 * d for xp, d in zip(xp, dfun(xp)))
         pp = fun(xp)
         if cond(pp, p, sqdiff(xp, y) / (2 * s2), sqdiff(x, yp) / (2 * s2)):
             x, p, y = xp, pp, yp
@@ -203,7 +203,7 @@ def tmcmc(fun, draws, lo, hi, beta=1, return_evidence=False, trace=False):
     p = 0
     S = 0
     d = len(lo)
-    x = [[random.uniform(l, h) for l, h in zip(lo, hi)] for i in range(draws)]
+    x = [tuple(random.uniform(l, h) for l, h in zip(lo, hi)) for i in range(draws)]
     f = np.array([fun(e) for e in x])
     x2 = [[None] * d for i in range(draws)]
     sigma = [[None] * d for i in range(d)]
@@ -246,7 +246,7 @@ def tmcmc(fun, draws, lo, hi, beta=1, return_evidence=False, trace=False):
         accept = 0
         for i, j in enumerate(ind):
             delta = [random.gauss(0, 1) for k in range(d)]
-            xp = [a + b for a, b in zip(x[j], sqrtC @ delta)]
+            xp = tuple(a + b for a, b in zip(x[j], sqrtC @ delta))
             if inside(xp):
                 fp = fun(xp)
                 if fp > f[j] or p * fp > p * f[j] + math.log(
