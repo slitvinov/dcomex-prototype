b3f8f79f89a603363f276b496c0fbdbde6c1c6c5
Sergey Litvinov <slitvinov@gmail.com>
2023-05-03 22:01:44 -0400

    update bio.sh; bio1.py dump only for rank = 0

--- bin/bio.sh
+++ bin/bio.sh
@@ -3,8 +3,12 @@
 : ${mph=%mph%}
 Verbose=0
 Config=0
+Surrogate=0
 while :
 do case "x$1" in
+       x-s) Surrogate=1
+	    shift
+	    ;;
        x-v) Verbose=1
 	    shift
 	    ;;
@@ -12,15 +16,26 @@ do case "x$1" in
 	    shift
 	    ;;
        x-h) cat >&2 <<'!'
-Usage: bio [-v] [-c] k1 mu time
-MSolve simulation of tumor grouth
-   k1       growth rate, 1/second
-   mu       shear modulus, kPa
-   time     timestep in days (positive integer)
+Usage: bio [-v] [-c] [-s] k1 mu time
+
+MSolve simulation of tumor growth.
+
+Positional arguments:
+  k1                   The growth rate of the tumor, in units of 1/second.
+  mu                   The shear modulus of the tumor, in units of kPa.
+  time                 The timestep to use in the simulation, in units of days.
+
 Options:
-  -v        verbose output
-  -c        output MSolve confiuration file and exit
-  -h        display help and exit
+  -v                   Print verbose output during the simulation.
+  -c                   Output the MSolve configuration file and exit.
+  -s                   Use a surrogate instead of the full simulation.
+  -h                   Display this help message and exit.
+Environment Variables:
+
+  mph                  The path to the mesh of the simulated domain,
+		       in .mph format.
+Returns:
+  The volume of the tumor, in units of cubic millimeters.
 !
 	    exit 2
 	    ;;
@@ -44,6 +59,23 @@ case "$time" in
     *) echo 'bio: error: time should be an positive integer' >&2; exit 2;;
 esac
 
+case $Surrogate in
+    1)
+	awk '
+	BEGIN {
+	    a = -1.4943643504045041e-07
+	    b = -1.4906646615767147e-07
+	    ab = 5.1441366269052818e-05
+	    c = 1.4200529488187354e-08
+	    d = 0.03489861972176718
+	    x = a * '"$k1"' + b * '"$mu"' + ab * '"$k1"' * '"$mu"' + c
+	    printf "%.16e\n", d * x * '"$time"'
+	}'
+	exit
+	;;
+esac
+
+
 if ! test -f "$mph"
 then printf "bio: error: cannot find mesh '%s'\n" "$mph" >&2
      exit 2
@@ -53,20 +85,20 @@ r=/tmp/result.$$.xml
 trap 'rm -f $c $r; exit 2' 1 2 15
 cat <<! > $c
 <MSolve4Korali version="1.0">
-        <Mesh>
-                <File>$mph</File>
-        </Mesh>
-        <Physics type="TumorGrowth">
-                <Time>$time</Time>
-                <Timestep>1</Timestep>
-        </Physics>
-        <Output>
-                <TumorVolume/>
-        </Output>
-        <Parameters>
-                <k1>$k1</k1>
-                <mu>$mu</mu>
-        </Parameters>
+	<Mesh>
+		<File>$mph</File>
+	</Mesh>
+	<Physics type="TumorGrowth">
+		<Time>$time</Time>
+		<Timestep>1</Timestep>
+	</Physics>
+	<Output>
+		<TumorVolume/>
+	</Output>
+	<Parameters>
+		<k1>$k1</k1>
+		<mu>$mu</mu>
+	</Parameters>
 </MSolve4Korali>
 !
 
--- examples/graph/bio1.py
+++ examples/graph/bio1.py
@@ -7,7 +7,6 @@ import mpi4py.MPI
 def fun(x):
     time = 3
     k1, mu = x
-    sys.stderr.write("%s\n" % x)
     command = ["bio", "-s", "%.16e" % k1, "%.16e" % mu, "%d" % time]
     try:
         output = subprocess.check_output(command, stderr=subprocess.STDOUT)
@@ -20,22 +19,25 @@ def fun(x):
     except ValueError:
         sys.stderr.write("bio.py: not a float '%s'\n" % output)
         exit(1)
-    sys.stderr.write("%g\n" % volume)
-    return -(volume / 1e-7 - 1)**2
+    sigma = 0.5
+    scale = 1e-6
+    sys.stderr.write("%g\n" % (volume/scale))
+    return -((volume / scale - 5.0)**2 / sigma**2)
 
 
 lo = (0.1, 1)
 hi = (0.5, 5)
 samples, S = graph.korali(fun,
-                          draws=10,
+                          draws=100,
                           lo=lo,
                           hi=hi,
                           return_evidence=True,
                           comm=mpi4py.MPI.COMM_WORLD)
-print("log evidence: ", S)
-plt.plot(*zip(*samples), 'o', alpha=0.5)
-plt.xlim(-5, 5)
-plt.ylim(-5, 5)
-plt.xlabel("a")
-plt.ylabel("b")
-plt.savefig("bio1.png")
+if  mpi4py.MPI.COMM_WORLD.Get_rank() == mpi4py.MPI.COMM_WORLD.Get_size() - 1:
+    print("log evidence: %g [%ld]" % (S, len(samples)))
+    plt.plot(*zip(*samples), 'o', alpha=0.5)
+    plt.xlim(lo[0], hi[0])
+    plt.ylim(lo[1], hi[1])
+    plt.xlabel("k1, 1/second")
+    plt.ylabel("mu, kPa")
+    plt.savefig("bio1.png")
